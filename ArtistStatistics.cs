c62435ee55ae29182e0e9a4b6d4178c175f17290
thelittlecitizen7 <thelittlecitizen7@gmail.com>
2020-08-13 17:51:51 +0300

    finish implemation functions

---
+++ ArtistStatistics.cs
@@ -0,0 +1,71 @@
+﻿using RadioStatistics.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RadioStatistics
+{
+    public class ArtistStatistics : IArtistStatistics
+    {
+        public IEnumerable<Artist> Artists { get; set; }
+        public ArtistStatistics(IEnumerable<Artist> artists)
+        {
+            Artists = artists;
+        }
+
+        // 4 
+        public IEnumerable<ArtistMetadata> GetArtistsAndAlbumCount()
+        {
+            return Artists.ToList().Select(a => new ArtistMetadata { Name = a.Name, AlbumCount = a.Albums.Count() });
+        }
+
+
+        // 2
+        public IEnumerable<Artist> GetArtistsOrderdByName()
+        {
+            return Artists.OrderBy(a => a.Name);
+        }
+
+
+        // 5
+        public Artist GetArtistWithMostAlbums()
+        {
+            return Artists.OrderByDescending(a => a.Albums.Count()).FirstOrDefault();
+        }
+
+        // 1
+        public IEnumerable<Artist> GetCatchyNamedArtists()
+        {
+            return Artists.Where(a => a.Name.Length < 7);
+        }
+
+        public IEnumerable<Artist> GetDiggingArtists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Artist> GetEligibleForIsraelArtists()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // 3
+        public Artist GetFirstArtistWithTwoAlbums()
+        {
+            return Artists.FirstOrDefault(a => a.Albums.Count() == 2);
+        }
+
+        public IEnumerable<Artist> GetSlackerArtists()
+        {
+            throw new NotImplementedException();
+        }
+
+        // 6
+        public IEnumerable<Artist> GetYoungArtists()
+        {
+            return Artists.Where(a => a.Albums.Count() > 0 && a.Albums.Count() <= 2);
+        }
+    }
+}
